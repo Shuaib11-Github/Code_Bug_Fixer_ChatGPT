b444158628a0384ff432854e9fd6fffd519f81ab
Mohammed Shuaib Iqbal <60477544+Shuaib11-Github@users.noreply.github.com>
2024-08-09 00:00:13 +0530

    Update app.py

--- Chapter04 CodeBugFixer/app.py
+++ Chapter04 CodeBugFixer/app.py
@@ -1,8 +1,8 @@
 """
 file name: app.py
-Description: Building a Code Bug Fixer App with ChatGPT API
+Description: Building a Code Bug Fixer App without ChatGPT API
 __copyright__ = "Copyright 2023, MartinYTech"
-__author__=  Martin Yanev
+__author__=  Mohammed Shuaib Iqbal
 __modified__= 20/04/2023
 """
 
